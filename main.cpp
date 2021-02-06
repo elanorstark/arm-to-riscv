20aa08127b9ff2eaff24696596954a8c3fcdc1d2
Elanor <els72@cam.ac.uk>
2021-02-06 13:46:32 +0000

    putting capstone stuff into main file

--- main.cpp
+++ main.cpp
@@ -7,8 +7,19 @@
 #include <cctype>
 #include <locale>
 
+#include <cstdio>
+#include <cinttypes>
+
 #include "Value.h"
 #include "Instruction.h"
+
+extern "C" {
+    #include "capstone/include/capstone/capstone.h"
+}
+
+extern "C" size_t CAPSTONE_API cs_disasm(csh ud, const uint8_t *buffer, size_t size, uint64_t offset, size_t count, cs_insn **insn);
+
+#define CODE "\xfd\x7b\xbe\xa9\xfd\x03\x00\x91\xe0\x1f\x00\xb9\xe0\x1f\x40\xb9\x1f\x04\x00\x71\x6c\x00\x00\x54\x20\x00\x80\x52\x07\x00\x00\x14\xe0\x1f\x40\xb9\x00\x04\x00\x51\xf6\xff\xff\x97\xe1\x03\x00\x2a\xe0\x1f\x40\xb9\x20\x7c\x00\x1b\xfd\x7b\xc2\xa8\xc0\x03\x5f\xd6"
 
 class Line {
 public:
@@ -26,39 +37,55 @@ static inline void ltrim(std::string &s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
 }
 
+
+cs_insn * main2(void)
+{
+    csh handle;
+    cs_insn *insn;
+    size_t count, j;
+    cs_regs regs_read, regs_write;
+    uint8_t read_count, write_count, i;
+
+    if (cs_open(CS_ARCH_ARM64, CS_MODE_ARM, &handle) != CS_ERR_OK)
+        return NULL;
+
+//    cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON); // slower but more detail
+
+    count = cs_disasm(handle, reinterpret_cast<const uint8_t *>(CODE), sizeof(CODE) - 1, 0x1000, 0, &insn);
+
+    // testing - print assembly instructions
+    if (count > 0) {
+        for (j = 0; j < count; j++) {
+            // Print assembly
+            printf("%X: %s\t%s\n", 0x1000+j, insn[j].mnemonic, insn[j].op_str);
+        }
+
+//        cs_free(insn, count);
+    } else
+        printf("ERROR: Failed to disassemble given code!\n");
+
+    cs_close(&handle);
+
+    return insn;
+}
+
 int main() {
 
-    // create the registers
-    Register::registers[0].set(1); // test line
-    std::cout << Register::registers[0].get() << ", " << Register::registers[1].get() << "\n"; // test line
+    cs_insn *insn = main2(); // disassembler
+
+    printf("%s\n", insn[0].mnemonic);
 
     std::string myText;
     std::ifstream CodeFile("arm_examples/factorial.txt");
     if (!CodeFile.is_open()) {
         return 1;
     }
-    std::vector<Line> lines;
-    std::string splitOn = " ";
-    while (std::getline(CodeFile, myText)) {
-        ltrim(myText); // removes whitespace from start of line
-        int current_index = myText.find(splitOn); // find next space
-        std::string mnc; // identify what type of instruction it is
-        if ((current_index < 0 && myText[myText.length() - 1] == ':') ||
-            (current_index > 0 && myText[current_index - 1] == ':')) {
-            mnc = "label";
-        } else {
-            mnc = myText.substr(0, current_index);
-        }
-        lines.emplace_back(Line(myText, mnc));
-    }
-    for (auto & line : lines) {
-        std::cout << line.mnc << ": " << line.lineText << "\n";
-    }
 
-    Instruction** instructions = new Instruction*[1];
-    instructions[0] = new Add(&Register::registers[0],new Const(2),new Const(3));
+    // create add instruction using objects
+    Instruction **instructions = new Instruction *[1];
+    instructions[0] = new Add(&Register::registers[0], new Const(2), new Const(3));
     instructions[0]->run();
-    std::cout << Register::registers[0].get();
+    std::cout << "result of example add: " << Register::registers[0].get();
 
     // close the file
     CodeFile.close();
--- test.c
+++ test.c
@@ -1,26 +1,105 @@
 /* test1.c */
+//http://www.capstone-engine.org/lang_c.html
 
 #include <stdio.h>
 #include <inttypes.h>
 
 #include "capstone/include/capstone/capstone.h"
 
-#define CODE "\x20\x00\x80\x52\x41\x00\x80\x52\x20\x00\x00\x0B\xC0\x03\x5F\xD6"
+#define CODE "\x40\x00\x01\x4b"
+//"\x20\x00\x80\x52\x41\x00\x80\x52\x20\x00\x00\x0B\xC0\x03\x5F\xD6"
+
+
+
+//int main(void)
+//{
+//    csh handle;
+//    cs_insn *insn;
+//    size_t count, j;
+//    cs_regs regs_read, regs_write;
+//    uint8_t read_count, write_count, i;
+//
+//    if (cs_open(CS_ARCH_ARM64, CS_MODE_ARM, &handle) != CS_ERR_OK)
+//        return -1;
+//    cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
+//    count = cs_disasm(handle, CODE, sizeof(CODE)-1, 0x1000, 0, &insn);
+//    if (count > 0) {
+//        size_t j;
+//        for (j = 0; j < count; j++) {
+//            //printf("%s\n",insn[j].op_str);
+//        }
+//
+//        if (cs_regs_access(handle, &insn[j],
+//                           regs_read, &read_count,
+//                           regs_write, &write_count) == 0) {
+//            if (read_count > 0) {
+//                printf("\n\tRegisters read:");
+//                for (i = 0; i < read_count; i++) {
+//                    printf(" %s", cs_reg_name(handle, regs_read[i]));
+//                }
+//                printf("\n");
+//            }
+//
+//            if (write_count > 0) {
+//                printf("\n\tRegisters modified:");
+//                for (i = 0; i < write_count; i++) {
+//                    printf(" %s", cs_reg_name(handle, regs_write[i]));
+//                }
+//                printf("\n");
+//            }
+//        }
+//
+//
+//
+//    cs_free(insn, count);
+//    } else {
+//        printf("ERROR: Failed to disassemble given code!\n");
+//    }
+//
+//    cs_close(&handle);
+//
+//    return 0;
+//}
 
 int main(void)
 {
     csh handle;
     cs_insn *insn;
-    size_t count;
+    size_t count, j;
+    cs_regs regs_read, regs_write;
+    uint8_t read_count, write_count, i;
 
     if (cs_open(CS_ARCH_ARM64, CS_MODE_ARM, &handle) != CS_ERR_OK)
         return -1;
+
+    cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
+
     count = cs_disasm(handle, CODE, sizeof(CODE)-1, 0x1000, 0, &insn);
     if (count > 0) {
-        size_t j;
         for (j = 0; j < count; j++) {
-            printf("0x%"PRIx64":\t%s\t\t%s\n", insn[j].address, insn[j].mnemonic,
-                    insn[j].op_str);
+            // Print assembly
+            printf("%s\t%s\n", insn[j].mnemonic, insn[j].op_str);
+
+            // Print all registers accessed by this instruction.
+            if (cs_regs_access(handle, &insn[j],
+                               regs_read, &read_count,
+                               regs_write, &write_count) == 0) {
+                if (read_count > 0) {
+                    printf("\n\tRegisters read:");
+                    for (i = 0; i < read_count; i++) {
+                        printf(" %s", cs_reg_name(handle, regs_read[i]));
+                    }
+                    printf("\n");
+                }
+
+                if (write_count > 0) {
+                    printf("\n\tRegisters modified:");
+                    for (i = 0; i < write_count; i++) {
+                        printf(" %s", cs_reg_name(handle, regs_write[i]));
+                    }
+                    printf("\n");
+                }
+            }
         }
 
         cs_free(insn, count);
