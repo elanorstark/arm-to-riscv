99a2cdca45f53fe9b02da8fe4501c4c11a0aec53
Elanor <els72@cam.ac.uk>
2021-02-17 17:19:06 +0000

    spsr registers class set up

--- Value.cpp
+++ Value.cpp
@@ -39,3 +39,37 @@ void Const::set(uint64_t _) {
 uint64_t Const::get() {
     return this->value;
 }
+
+std::map<std::string, SPSR_Register*> SPSR_Register::names { {"n", &SPSR_Register::n},
+                                                             {"c", &SPSR_Register::c},
+                                                             {"z", &SPSR_Register::z},
+                                                             {"v", &SPSR_Register::v},
+                                                             {"ss", &SPSR_Register::ss},
+                                                             {"il", &SPSR_Register::il},
+                                                             {"d", &SPSR_Register::d},
+                                                             {"a", &SPSR_Register::a},
+                                                             {"i", &SPSR_Register::i},
+                                                             {"f", &SPSR_Register::f},
+                                                             {"m", &SPSR_Register::m}};
+
+uint64_t SPSR_Register::get() {
+    return this->value;
+}
+
+
+void SPSR_Register::set_by_name(std::string name, uint64_t value) {
+    uint8_t one_bit = 1 & value;
+    SPSR_Register::names[name]->set(one_bit);
+}
+
+uint64_t SPSR_Register::get_by_name(std::string name){
+    return SPSR_Register::names[name]->get();
+}
+
+SPSR_Register::SPSR_Register() {
+    this->value = 0;
+}
+
+void SPSR_Register::set(uint64_t value) {
+    this->value = value;
+}
--- Value.h
+++ Value.h
@@ -2,6 +2,7 @@
 #define CODE_VALUE_H
 
 #include <cstdint>
+#include <map>
 
 class Value {
 public:
@@ -15,7 +16,7 @@ public:
 
     uint64_t contents{0};
 
-    void set(uint64_t value) override;
+    void set(uint64_t value);
 
     uint64_t get();
 
@@ -26,6 +27,36 @@ public:
     static Register pc;
 
     static Register sp;
+};
+
+class SPSR_Register: public Value {
+public:
+    static std::map<std::string, SPSR_Register*> names;
+
+    uint64_t value;
+
+    uint64_t get();
+
+    void set(uint64_t);
+
+    SPSR_Register();
+
+    static uint64_t get_by_name(std::string name);
+
+    static void set_by_name(std::string name, uint64_t value);
+
+    static SPSR_Register n;
+    static SPSR_Register z;
+    static SPSR_Register c;
+    static SPSR_Register v;
+    static SPSR_Register ss;
+    static SPSR_Register il;
+    static SPSR_Register d;
+    static SPSR_Register a;
+    static SPSR_Register i;
+    static SPSR_Register f;
+    static SPSR_Register m;
+
 };
 
 class Const: public Value {
